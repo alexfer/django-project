3561a0b7a02eff061bc881f9e221e8114a5e5e8e
Alexander Sh <alexander@probegin.com>
2022-11-18 15:43:37 +0200

    form hanle implementation

--- content/urls.py
+++ content/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('accounts/content/change/<int:id>/', views.change, name='change-entry'),
     path('accounts/content/destroy/<int:id>/', views.destroy, name='destroy-entry'),
     path('accounts/content/create/', views.create, name='create-entry'),
+    path('accounts/content/add/', views.CreateEntry, name='add-entry'),
+    path('accounts/content/edit/<int:id>/', views.ChangeEntry, name='edit-entry'),
     path('accounts/content/collection/', views.collection, name='collection-entries'),
 ]
--- content/views.py
+++ content/views.py
@@ -1,14 +1,50 @@
 from datetime import datetime
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from content.models import Entry
 from django.contrib import messages
 from comment.models import Comment
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext_lazy as _
+from .forms import CreateEntryForm, ChangeEntryForm
 
 
-# Create your views here.
+@login_required
+def CreateEntry(request):
+    if request.method == 'POST':
+        form = CreateEntryForm(request.POST)
+        if form.is_valid():
+            entry = form.save(commit=False)
+            entry.user_id = request.user.id
+            entry.save()
+            return redirect('details', entry.id)
+    else:
+        form = CreateEntryForm()
+
+    return render(request, 'content/entry-form.html', {
+        'form': form,
+        'title': _('Create Entry'),
+    })
+
+
+@login_required
+def ChangeEntry(request, id):
+
+    entry = get_object_or_404(Entry, user_id=request.user.id, id=id)
+
+    if request.method == 'POST':
+        form = ChangeEntryForm(request.POST, instance=entry)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('edit-entry', entry.id)
+    else:
+        form = ChangeEntryForm(instance=entry)
+
+    return render(request, 'content/entry-form.html', {
+        'form': form,
+        'title': _('Change Entry'),
+    })
+
 
 def details(request, id):
     comments = Comment.objects.filter(entry_id=id, approved=True).order_by('-created_at')
@@ -95,6 +131,7 @@ def change(request, id):
             'entry': entry,
             'title': _('Change'),
         })
+
 
 @login_required
 def destroy(request, id):
