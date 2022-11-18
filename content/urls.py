ace62fa2ffe3d868e5d17df77f0e03e0b1b10d3c
Alexander Sh <alexander@probegin.com>
2022-11-18 16:41:07 +0200

    create footer

--- content/urls.py
+++ content/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 urlpatterns = [
     path('content/<int:id>/', views.details, name='details'),
-    path('accounts/content/change/<int:id>/', views.change, name='change-entry'),
+    path('accounts/content/change/<int:id>/', views.ChangeEntry, name='change-entry'),
     path('accounts/content/destroy/<int:id>/', views.destroy, name='destroy-entry'),
-    path('accounts/content/create/', views.create, name='create-entry'),
-    path('accounts/content/add/', views.CreateEntry, name='add-entry'),
-    path('accounts/content/edit/<int:id>/', views.ChangeEntry, name='edit-entry'),
+    path('accounts/content/create/', views.CreateEntry, name='create-entry'),
     path('accounts/content/collection/', views.collection, name='collection-entries'),
 ]
--- content/views.py
+++ content/views.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from content.models import Entry
 from django.contrib import messages
@@ -37,7 +36,7 @@ def ChangeEntry(request, id):
         if form.is_valid():
             messages.success(request, _('Entry successfully updated.'), extra_tags='alert alert-success')
             form.save(commit=True)
-            return redirect('edit-entry', entry.id)
+            return redirect('change-entry', entry.id)
     else:
         form = ChangeEntryForm(instance=entry)
 
@@ -64,27 +63,6 @@ def details(request, id):
 
 
 @login_required
-def create(request):
-    if request.method == 'POST':
-        content = request.POST['content']
-        title = request.POST['title']
-        if not title or not content:
-            messages.error(request, _('All fields cannot be empty.'), extra_tags='alert alert-danger')
-            return render(request, 'content/form.html', {
-                'title': _('Create'),
-            })
-        else:
-            entry = Entry(title=title, content=content)
-            entry.user_id = request.user.id
-            entry.save()
-            return redirect('collection-entries')
-    else:
-        return render(request, 'content/form.html', {
-            'title': _('Create'),
-        })
-
-
-@login_required
 def collection(request):
     entries = Entry.objects.filter(user_id=request.user.id).order_by('-id')
     paginator = Paginator(entries, 20)
@@ -94,44 +72,6 @@ def collection(request):
     return render(request, 'content/collection.html', {
         'entries': context,
     })
-
-
-@login_required
-def change(request, id):
-    try:
-        entry = Entry.objects.get(user_id=request.user.id, id=id)
-    except Entry.DoesNotExist:
-        return render(request, 'errors/404.html', status=404)
-
-    if request.method == 'POST':
-        content = request.POST['content']
-        title = request.POST['title']
-        created_at = request.POST['created_at']
-
-        try:
-            datetime.strptime(created_at, '%Y-%m-%d %H:%M')
-        except ValueError:
-            messages.error(
-                request,
-                _('Incorrect date format, should be YYYY-MM-DD HH:MM'),
-                extra_tags='alert alert-danger'
-            )
-            return redirect('change-entry', id=id)
-        if not title or not content:
-            messages.error(request, _('All fields cannot be empty.'), extra_tags='alert alert-danger')
-            return render(request, 'content/form.html', {})
-        else:
-            entry.title = title
-            entry.content = content
-            entry.created_at = datetime.strptime(created_at, "%Y-%m-%d %H:%M")
-            entry.updated_at = datetime.now()
-            entry.save()
-            return redirect('collection-entries')
-    else:
-        return render(request, 'content/form.html', {
-            'entry': entry,
-            'title': _('Change'),
-        })
 
 
 @login_required
